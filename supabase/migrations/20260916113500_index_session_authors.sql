555598469cfe3b2386d5865240a94787dd9a4d28
afternoonacademy <admin@theafternoonacademy.com>
2026-09-16 13:57:31 +0200

    Restore session capacity workflow files

--- supabase/migrations/20260916113500_index_session_authors.sql
+++ supabase/migrations/20260916113500_index_session_authors.sql
@@ -1,1 +1,2 @@
-sed: -e expression #1, char 2: unexpected `,'
+create index academy_sessions_created_by_idx on public.academy_sessions(created_by);
+create index session_placements_created_by_idx on public.session_placements(created_by);

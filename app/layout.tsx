b8ee9bf5f2be71fd18722f7e8796177b478d4af8
afternoonacademy <admin@theafternoonacademy.com>
2026-09-23 09:19:55 +0200

    chore: add Google Analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
+import Script from "next/script"
 
 import { Toaster } from "@/components/ui/sonner"
 
@@ -80,6 +81,16 @@ export default function RootLayout({
         {children}
         <Toaster richColors position="top-center" />
         <Analytics />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-YRGHJMDL58"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag("js", new Date());
+gtag("config", "G-YRGHJMDL58");`}
+        </Script>
       </body>
     </html>
   )
